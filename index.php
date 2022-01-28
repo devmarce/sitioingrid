923ee68c5421e063e2d2c015d244c7c849ec6e37
Marcelo Mareco <marcelo_mareco@div-it.com.ar>
2022-01-28 01:12:21 -0300

    inicial

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel="stylesheet" href="./css/main.css">
     <title>ingridulce</title>
 </head>
 <body>
-    <dvi class="container-gral">
-        
-    </dvi>
+    <div class="container-gral">
+        <div class="marcodiv fondodiv" style="width: 99%; height: 400px; margin: auto">
+        </div>
+    </div>
 </body>
 </html>
