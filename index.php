21a835cdd60c3a52b5d742ddb98ed01ea31d2c24
Marcelo Mareco <marcelo_mareco@div-it.com.ar>
2022-01-28 13:17:48 -0300

    add metas

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta name="description" content="descripcion del sitio"><!--agregar descripcion de la pagina-->
+<!--    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">-->
     <link rel="stylesheet" href="./css/main.css">
     <title>ingridulce</title>
 </head>
