37331428c8b2ee547f6b05e09a6785dcc2783878
marcelo <98444716+devmarce@users.noreply.github.com>
2025-09-29 09:52:25 -0300

    add Favicon

---
+++ componentes/head.php
@@ -0,0 +1,27 @@
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Dulcing Pastelería</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- favicons -->
+  <?php include_once "./componentes/favicon-files.php"; ?>
+
+  <!-- styles css -->
+  <link rel="stylesheet" href="./css/style.css">
+
+  <meta name="msapplication-TileColor" content="#03e71f">
+  <meta name="msapplication-TileImage" content="/img/favicon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#03e71f">
+
+  <meta property="og:locale" content="es_AR">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Dulcing - Pastelería">
+  <meta property="og:description" content="Dulces caseros, tortas personalizadas y postres artesanales para compartir momentos especiales.">
+  <meta property="og:url" content="https://dulcing.com.ar/">
+  <meta property="og:image" content="https://dulcing.com.ar/img/logos/Dulcing.png">
+  <meta property="og:image:secure_url" content="https://dulcing.com.ar/img/logos/Dulcing.png">
+  <meta property="og:site_name" content="Dulcing Pastelería">
+
+  <!-- Boostrap scripts -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
